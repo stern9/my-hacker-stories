030df704d99b32dadc05b6b9d68897ed006e6dbc
stern9 <avram.stern@digitas.com>
2021-03-19 20:50:15 -0600

    adding styles, pending styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect, useRef, useReducer, useCallback } from "react";
-import "./App.css";
+import styles from "./App.module.css";
+import cs from "classnames";
 
 const API_ENDPOINT = `http://hn.algolia.com/api/v1/search?query=`;
 
@@ -49,7 +50,7 @@ const useSemiPersistentState = (key, initialState) => {
 
 const App = () => {
   const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-    <form onSubmit={onSearchSubmit}>
+    <form onSubmit={onSearchSubmit} className={styles.searchForm}>
       <InputWithLabel
         id="search"
         value={searchTerm}
@@ -58,7 +59,11 @@ const App = () => {
       >
         <strong>Search:</strong>
       </InputWithLabel>
-      <button type="submit" disabled={!searchTerm}>
+      <button
+        type="submit"
+        disabled={!searchTerm}
+        className={cs(styles.button, styles.buttonLarge)}
+      >
         Submit
       </button>
     </form>
@@ -112,14 +117,13 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <hr />
       {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading ? (
         <p>Loading...</p>
@@ -148,7 +152,9 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className={styles.label}>
+        {children}
+      </label>
       &nbsp;
       <input
         ref={inputRef}
@@ -156,6 +162,7 @@ const InputWithLabel = ({
         type={type}
         value={value}
         onChange={onInputChange}
+        className={styles.input}
       />
     </>
   );
@@ -168,15 +175,19 @@ const List = ({ list, onRemoveItem }) =>
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <div>
-      <span>
+    <div className={styles.item}>
+      <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span> {item.author}</span>
-      <span> {item.num_comments}</span>
-      <span> {item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: "30%" }}> {item.author}</span>
+      <span style={{ width: "10%" }}> {item.num_comments}</span>
+      <span style={{ width: "10%" }}> {item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          className={`${styles.button}${styles.buttonSmall}`}
+        >
           Dismiss
         </button>
       </span>
